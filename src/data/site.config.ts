a1d417e6618257cfca499d6837f8c7cd010230ee
deraires <148113642+deraires@users.noreply.github.com>
2024-03-10 14:58:29 +0100

    Cambios autor e idioma

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	author: 'Dera Irés Cortez', // Site author
+	title: 'Innovacion 360', // Site title.
+	description: 'Descubre las últimas novedades en gadgets y tecnología. Mantente al día con las innovaciones en electrónica, video juegos, móviles, tablets e informática en nuestra plataforma.', // Description to display in the meta tags
+	lang: 'en-GB', 'es-ES',
+	ogLocale: 'en_GB', 'es_ES',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
